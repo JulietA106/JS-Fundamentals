4b0b5b6b91195d1d22c21f990984576b654e26f7
Juliet Angaandi <julietangaandi93@gmail.com>
2025-09-12 20:34:05 +0000

    Add multi-language script using array and loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+// Array of languages and messages
+const messages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Loop through the array and print each message
+for (let i = 0; i < messages.length; i++) {
+  console.log(messages[i]);
+}
