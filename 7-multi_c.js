afba49c6e5152ea9778e643083e14cdf5b12aac5
Juliet Angaandi <julietangaandi93@gmail.com>
2025-09-13 12:50:41 +0000

    Fix script to handle negative and missing arguments correctly

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,15 +1,11 @@
-// Get the first argument (number of occurrences)
-const arg = process.argv[2];
+#!/usr/bin/node
+const x = parseInt(process.argv[2]);
 
-// Convert it to an integer
-const count = parseInt(arg, 10);
-
-// Check if count is a valid positive number
-if (isNaN(count) || count <= 0) {
-  console.log("Missing number of occurrences");
+if (isNaN(x) || x < 1) {
+  console.log('Missing number of occurrences');
 } else {
-  // Loop to print "C is fun" count times
-  for (let i = 0; i < count; i++) {
-    console.log("C is fun");
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
   }
 }
+
